fcff94d779de4fbead5897311561dcf478ddc1ba
Ubuntu <ubuntu@ip-172-31-93-33.ec2.internal>
2021-05-28 17:25:26 +0000

    A program that makes way yo many numbers

---
+++ RGB_too_many.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Csiffary
+# Created on: May 28, 2021
+# This program lists a butt load of numbers, in the form of RGB numbers
+# theres going to be way to many numbers itll probably crash my computer
+
+
+# so that i can have the dramatic delay
+import time
+
+
+def main():
+    # make sure all the counters are 0
+    counterRed = 0
+    counterBlue = 0
+    counterGreen = 0
+
+    # the delay and warning
+    print("It may take a while to get through them all")
+    print("starting in 3")
+    time.sleep(1)
+    print("starting in 2")
+    time.sleep(1)
+    print("starting in 1")
+    time.sleep(1)
+    # the blob of for loops, all the stuff happens here
+    for counterRed in range(0, 255):
+        for counterGreen in range(0, 255):
+            for counterBlue in range(0, 255):
+                print("{0}, {1}, {2}".format(counterRed,
+                      counterGreen, counterBlue))
+
+
+if __name__ == "__main__":
+    main()
